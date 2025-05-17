dcb0de0cb6735ba30e1c0d9ca62b39044e98f0d1
ahmadh-najahi <ahmednajahi2003@gmail.com>
2025-05-17 12:12:24 +0530

    fix: uncomment Firebase auth import in Signup component

--- src/assets/pages/Signup.jsx
+++ src/assets/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-// import { auth } from '../firebase';
+import { auth } from '../firebase';
 import { useNavigate, Link } from 'react-router-dom';
 
 export default function Signup() {
